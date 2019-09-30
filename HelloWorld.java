b8e42e9424b1b87a1ff26d054112ab87762eeb0f
Manu Jadli <majadli2@in.ibm.com>
2019-09-30 08:40:56 +0530

    Adding code base

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {	
+			System.out.println("********************* Hello and welcome to The United States Of America ***************************");
+	}
+
+}
